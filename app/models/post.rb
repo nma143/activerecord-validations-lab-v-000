9e35cbf87e6eb0fcbb2bd6cee03005700e786dad
Nikki A <nikki.apostolakis@gmail.com>
2019-01-28 01:58:16 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fictio)}
+  validate :check_clickbait
+
+  def check_clickbait
+    clickbait_phrases = [/Won't Believe/, /Secret/, /Top \d+/, /Guess/]
+    if clickbait_phrases.none? {|regx| regx.match(title)}
+      errors.add(:title, "is not sufficiently clickbait-y")
+    end
+  end
 end
