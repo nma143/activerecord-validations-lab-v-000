74156c169206ecca3adb228f1643f3a97e6b6481
Nikki A <nikki.apostolakis@gmail.com>
2019-01-28 01:49:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fictio)}
+  validate :check_clickbait
+
+  def check_clickbait
+    clickbait_phrases = [/Won't Believe/, /Secret/, /Top \d+/, /Guess/]
+    errors.add(:title, "is not sufficiently clickbait-y")
+  end
 end
